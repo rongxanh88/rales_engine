b48928f036ff7fd63baa5272eb67279466f37a92
MasonHolland <masontholland@gmail.com>
2017-06-27 10:44:42 -0600

    factories completed

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,12 +1,9 @@
 FactoryGirl.define do
   factory :invoice do
-<<<<<<< HEAD
 
-=======
     association :merchant, factory: :merchant
     association :customer, factory: :customer
-    
+
     status 'shipped'
->>>>>>> 99983b38687cfef3bc12e2e28b793562c4e229e0
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :item do
-
+    name        'Shovel'
+    description 'New item made of sturdy materials.'
+    unit_price  10
+    association :merchant, factory: :merchant
   end
 end
