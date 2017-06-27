5bc92bc2ba6bd94ac9984c05ba14f375874e19c1
rongxanh88 <rongxanh88@gmail.com>
2017-06-27 08:27:18 -0600

    pass model specs for merchant, customer, transaction

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'valid attributes' do
-    it { should_validate_presence_of(:name) }
+    it { should validate_presence_of(:name) }
   end
 
   describe 'relationships' do
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,15 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe 'valid attributes' do
-    it { should_validate_presence_of(:credit_card_number) }
-    it { should_validate_presence_of(:result) }
+    it { should validate_presence_of(:credit_card_number) }
+    it { should validate_presence_of(:result) }
   end
 
   describe 'relationships' do
-    it 'belongs to invoice' do
-      transaction = create(:transaction)
-
-      expect(transaction).to respond_to(Invoice)
-    end
+    it { belong_to(:invoice) }
   end
 end
