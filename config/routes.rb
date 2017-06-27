9ef361ac425fd1ef84710aba020b2a638261b426
MasonHolland <masontholland@gmail.com>
2017-06-27 13:29:54 -0600

    added routes for items

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,29 @@ Rails.application.routes.draw do
         get 'find?',      to: 'find#show'
       end
 
+      namespace :items do
+        get '',                       to: 'items#index'
+        get ':id',                    to: 'items#show'
+        get 'find_all?',              to: 'find#index'
+        get 'find?',                  to: 'find#show'
+        get 'most_revenue?quantity=', to: 'business#show'
+      end
+
+      Mason
+      GET /api/v1/merchants/:id/customers_with_pending_invoices
+      GET /api/v1/merchants/:id/favorite_customer
+      GET /api/v1/items/most_revenue?quantity=x
+      GET /api/v1/merchants/revenue?date=x
+      GET /api/v1/merchants/most_revenue?quantity=x
+
+      Bao
+      GET /api/v1/merchants/:id/revenue
+      GET /api/v1/merchants/:id/revenue?date=x
+      GET /api/v1/merchants/most_items?quantity=x
+      GET /api/v1/customers/:id/favorite_merchant
+      GET /api/v1/items/:id/best_day
+      GET /api/v1/items/most_items?quantity=x
+
       resources :merchants, only: [:index, :show]
     end
   end
