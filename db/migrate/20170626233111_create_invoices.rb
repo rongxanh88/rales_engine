c236c9a37923ca158e8fb53e7c9ca93f4d0459ac
rongxanh88 <rongxanh88@gmail.com>
2017-06-26 18:14:17 -0600

    create rake task to import all six csvs

--- db/migrate/20170626233111_create_invoices.rb
+++ db/migrate/20170626233111_create_invoices.rb
@@ -2,8 +2,8 @@ class CreateInvoices < ActiveRecord::Migration[5.1]
   def change
     create_table :invoices do |t|
       t.text :status
-      t.references :merchants, index: true, foreign_key: true
-      t.references :customers, index: true, foreign_key: true
+      t.references :merchant, index: true, foreign_key: true
+      t.references :customer, index: true, foreign_key: true
 
       t.timestamps
     end
