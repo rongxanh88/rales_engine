e71364ecd26be72763b8dcf88e2ff5266d7bcf89
rongxanh88 <rongxanh88@gmail.com>
2017-06-27 11:07:49 -0600

    redid routes and namespaced routes for find and random api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+
+      namespace :merchants do
+        get '', to: 'merchants#index'
+        get ':id', to: 'merchants#show'
+        get 'find_all?', to: 'find#index'
+        get 'find?', to: 'find#show'
+        get 'random', to: 'random#show'
+      end
+
     end
   end
 end
