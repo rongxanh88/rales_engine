3824b92bcd6308633e279d00a1281d9ea3188b88
MasonHolland <masontholland@gmail.com>
2017-06-28 17:32:41 -0600

    most revenue for merchants completed

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::Items::MostRevenueController < ApplicationController
   def index
     # most_revenue?quantity=x
     # returns the top x merchants ranked by total revenue
-    Merchant.select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue").joins(:invoice_items).group("merchants.id").order("total_revenue DESC").limit(5)
+    Merchant.joins(
+      "INNER JOIN (" +
+      Invoice.joins(:transactions, :invoice_items).where(transactions: {result: 'success'})
+      .select("invoices.merchant_id, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+      .group("invoices.merchant_id")
+      .order("total_revenue DESC")
+      .limit(5)
+      .to_sql +
+      ") merchant_revenues ON merchant_revenues.merchant_id = merchants.id")
+      .select("merchant_revenues.total_revenue, merchants.*")
   end
 end
