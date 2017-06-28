3af34c349387cdd786bd3c7ab4f4b97903f08e2b
MasonHolland <masontholland@gmail.com>
2017-06-27 22:36:21 -0600

    test written. may need to change query however

--- app/controllers/api/v1/merchants/customer_controller.rb
+++ app/controllers/api/v1/merchants/customer_controller.rb
@@ -2,15 +2,13 @@ class Api::V1::Merchants::CustomerController < ApplicationController
 
   def show
     # favorite_customer
-    
+
   end
 
   def index
     #customers_with_pending_invoices
     binding.pry
-    pending = Customer.joins(:invoices)
-                      .where("invoices.merchant_id = ? AND invoices.status = ?",
-                      params[:merchant_id], 'pending')
+    pending = Customer.joins(:invoices).where("invoices.merchant_id = ? AND invoices.status = ?", params[:merchant_id], 'pending')
     render json: pending
   end
 
---
+++ spec/requests/api/v1/customers_pending_orders_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'customer_records_api', type: :request do
+  context 'when the merchant exists' do
+    it 'returns invoices with a status of pending' do
+      customer = create(:customer)
+      merchant = create(:merchant)
+      invoice1 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id, status: 'pending')
+      invoice2 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id, status: 'pending')
+
+      get "/api/v1/merchants/#{merchant.id}/customers_with_pending_invoices"
+      result = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(result.first["status"]).to eq("pending")
+    end
+  end
+
+  context 'when the date does not exist' do
+
+  end
+end
