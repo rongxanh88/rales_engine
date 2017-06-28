52a4878ed8be02cf38698d7d37ec29cc4345c13d
rongxanh88 <rongxanh88@gmail.com>
2017-06-28 17:15:15 -0600

    finish test for best day for item spec

--- spec/requests/api/v1/best_day_for_item_spec.rb
+++ spec/requests/api/v1/best_day_for_item_spec.rb
@@ -3,24 +3,23 @@ require 'rails_helper'
 RSpec.describe 'best day for item', type: :request do
   context 'when the data exists' do
     it 'returns best day' do
-      # customer = create(:customer)
-      # merchant = create(:merchant, name: "Saks and Raks")
-      # invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
-      # create(:transaction, invoice_id: invoice.id)
-
-      # 5.times do
-      #   create(:merchant)
-      # end
       date = DateTime.new(2015,3,20)
-      other_date = DateTime.new(2015,6,30)
+      other_date = DateTime.new(2015,1,30)
       item = create(:item)
       invoice = create(:invoice, created_at: date)
+      other_invoice = create(:invoice, created_at: other_date)
+      another_invoice = create(:invoice, created_at: other_date)
       create(:transaction, invoice_id: invoice.id)
+      create(:transaction, invoice_id: other_invoice.id)
+      create(:invoice_item, quantity: 50, invoice_id: invoice.id, item_id: item.id)
+      create(:invoice_item, quantity: 49, invoice_id: another_invoice.id, item_id: item.id)
+      create(:invoice_item, quantity: 50, invoice_id: other_invoice.id, item_id: item.id)
+
       get "/api/v1/items/#{item.id}/best_day"
       result = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
-      # expect(result["name"]).to eq("Saks and Raks")
+      expect(result["best_day"]).to eq("2015-03-20T00:00:00.000Z")
     end
   end
 
