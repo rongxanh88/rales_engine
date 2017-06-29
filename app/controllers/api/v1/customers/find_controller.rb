1ff96882aa47d9ac7e24b26205eb37aae35e6993
rongxanh88 <rongxanh88@gmail.com>
2017-06-28 21:32:05 -0600

    add show view for random merchant, transaction, and customer endpoints

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::Customers::FindController < ApplicationController
   def index
-    render json: Customer.where(search_params)
+    @customers = Customer.where(search_params)
   end
 
   def show
-    render json: Customer.find_by(search_params)
+    @customer = Customer.find_by(search_params)
   end
 
   private
