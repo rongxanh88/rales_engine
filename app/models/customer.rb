fa109208cc77620868bc2b45ab9557d269f9e72b
Bao Nguyen <rongxanh88@gmail.com>
2017-06-29 15:55:39 -0600

    formatted pending invoices sql

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -28,7 +28,11 @@ class Customer < ApplicationRecord
   end
 
   def self.pending_invoices(merchant_id)
-    Customer.joins("INNER JOIN (" + Invoice.joins("INNER JOIN (SELECT \"transactions\".\"invoice_id\" FROM \"transactions\" WHERE \"transactions\".\"result\" = 'failed' EXCEPT SELECT \"transactions\".\"invoice_id\" FROM \"transactions\" WHERE \"transactions\".\"result\" = 'success') pending_transactions ON pending_transactions.invoice_id = invoices.id")
-    .where("invoices.merchant_id = ?", merchant_id).to_sql + ") invoices ON invoices.customer_id = customers.id")
+    Customer.joins(
+      "INNER JOIN (" +
+        Invoice.joins(
+          "INNER JOIN (SELECT \"transactions\".\"invoice_id\" FROM \"transactions\" WHERE \"transactions\".\"result\" = 'failed' EXCEPT SELECT \"transactions\".\"invoice_id\" FROM \"transactions\" WHERE \"transactions\".\"result\" = 'success') pending_transactions ON pending_transactions.invoice_id = invoices.id"
+          ).where("invoices.merchant_id = ?", merchant_id).to_sql +
+        ") invoices ON invoices.customer_id = customers.id")
   end
 end
