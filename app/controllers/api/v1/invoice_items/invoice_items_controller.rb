cd463f2354c60bba686e661dbaa84273bd152e1a
rongxanh88 <rongxanh88@gmail.com>
2017-06-29 16:56:40 -0600

    fixed a couple of spec harness errors

--- app/controllers/api/v1/invoice_items/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items/invoice_items_controller.rb
@@ -5,5 +5,6 @@ class Api::V1::InvoiceItems::InvoiceItemsController < ApplicationController
 
   def show
     @invoice_item = InvoiceItem.find(params[:id])
+    @invoice_item.unit_price = @invoice_item.unit_price.to_f/100
   end
 end
